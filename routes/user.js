101ad8e41025bc374df2227e2b7e9370225ef4f5
Erman Konyar <er.konyar@gmail.com>
2023-04-30 22:15:16 +0300

    Fifth Commit

--- routes/user.js
+++ routes/user.js
@@ -1,52 +1,55 @@
-const express = require('express');
-const router =express.Router();
+const express = require("express");
+const router = express.Router();
 
-
-const data ={
-    title:"Popüler Kurslar",
-    categories:["Web Geliştirme","Programlama","Veri Analizi","Mobil Uygulamalar","Ofis Uygulamaları"],
-    blogs:[
+const data = {
+    title: "Popüler Kurslar",
+    categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"],
+    blogs: [
         {
-            blogid:1,
-            baslik:"Komple Uygulamalı Web Geliştirme",
-            aciklama:"Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
-            resim:"1.jpeg",
-            anasayfa:true
+            blogid: 1,
+            baslik: "Komple Uygulamalı Web Geliştirme",
+            aciklama: "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+            resim: "1.jpeg",
+            anasayfa: true,
+            onay: true
         },
-        {blogid:2,
-            baslik:"Komple Uygulamalı Web Geliştirme",
-            aciklama:"Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
-            resim:"2.jpeg",
-            anasayfa:false},
-        {blogid:3,
-            baslik:"Komple Uygulamalı Web Geliştirme",
-            aciklama:"Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
-            resim:"3.jpeg",
-            anasayfa:true},
-        {blogid:4,
-            baslik:"Komple Uygulamalı Web Geliştirme",
-            aciklama:"Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
-            resim:"4.jpeg",
-            anasayfa:false}
+        {
+            blogid: 2,
+            baslik: "Python ile Sıfırdan İleri Seviye Python Programlama",
+            aciklama: "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
+            resim: "2.jpeg",
+            anasayfa: true,
+            onay: false
+        },
+        {
+            blogid: 3,
+            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
+            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
+            resim: "3.jpeg",
+            anasayfa: false,
+            onay: true
+        },
+        {
+            blogid: 3,
+            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
+            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
+            resim: "3.jpeg",
+            anasayfa: false,
+            onay: true
+        },
     ]
 }
 
-router.use("/blogs/:blogid",(req,res,next)=>{
-    
-    res.render('../views/users/blog-details')
-    
+router.use("/blogs/:blogid", function(req, res) {
+    res.render("users/blog-details");
 });
 
-router.use("/blogs",(req,res,next)=>{
-    res.render('../views/users/blogs',data)
-    
+router.use("/blogs", function(req, res) {
+    res.render("users/blogs", data);
 });
 
-
-
-router.use("/",(req,res,next)=>{
-    res.render('../views/users/index',data)
-    
+router.use("/", function(req, res) {
+    res.render("users/index", data);
 });
 
-module.exports=router;
+module.exports = router;
