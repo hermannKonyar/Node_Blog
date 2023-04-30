8e30108db547bd0114dca808625264e404e6c47e
Erman Konyar <er.konyar@gmail.com>
2023-04-30 19:24:57 +0300

    Third Commit

--- routes/user.js
+++ routes/user.js
@@ -2,21 +2,26 @@ const express = require('express');
 const router =express.Router();
 
 
+const data ={
+    title:"Popüler Kurslar",
+    categories:["Web Geliştirme","Programlama","Veri Analizi","Mobil Uygulamalar","Ofis Uygulamaları"]
+}
+
 router.use("/blogs/:blogid",(req,res,next)=>{
     
-    res.sendFile('../views/users/blog-details')
+    res.render('../views/users/blog-details')
     
 });
 
 router.use("/blogs",(req,res,next)=>{
-    res.sendFile('../views/users/blogs')
+    res.render('../views/users/blogs')
     
 });
 
 
 
 router.use("/",(req,res,next)=>{
-    res.render('../views/users/index')
+    res.render('../views/users/index',data)
     
 });
 
