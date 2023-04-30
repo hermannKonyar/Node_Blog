f3060506663eecaf32b9b938b2170aa666bc315e
Erman Konyar <er.konyar@gmail.com>
2023-04-30 19:53:54 +0300

    Fourth Commit

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,31 @@ const router =express.Router();
 
 const data ={
     title:"Popüler Kurslar",
-    categories:["Web Geliştirme","Programlama","Veri Analizi","Mobil Uygulamalar","Ofis Uygulamaları"]
+    categories:["Web Geliştirme","Programlama","Veri Analizi","Mobil Uygulamalar","Ofis Uygulamaları"],
+    blogs:[
+        {
+            blogid:1,
+            baslik:"Komple Uygulamalı Web Geliştirme",
+            aciklama:"Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+            resim:"1.jpeg",
+            anasayfa:true
+        },
+        {blogid:2,
+            baslik:"Komple Uygulamalı Web Geliştirme",
+            aciklama:"Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+            resim:"2.jpeg",
+            anasayfa:false},
+        {blogid:3,
+            baslik:"Komple Uygulamalı Web Geliştirme",
+            aciklama:"Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+            resim:"3.jpeg",
+            anasayfa:true},
+        {blogid:4,
+            baslik:"Komple Uygulamalı Web Geliştirme",
+            aciklama:"Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+            resim:"4.jpeg",
+            anasayfa:false}
+    ]
 }
 
 router.use("/blogs/:blogid",(req,res,next)=>{
@@ -14,7 +38,7 @@ router.use("/blogs/:blogid",(req,res,next)=>{
 });
 
 router.use("/blogs",(req,res,next)=>{
-    res.render('../views/users/blogs')
+    res.render('../views/users/blogs',data)
     
 });
 
